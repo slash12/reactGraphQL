8b5d10e2915c39103be37fa85120e0255f765fdf
slash12 <ns@esokia-webagency.com>
2019-09-17 17:19:56 +0400

    post function with validation on Budget amount completed

--- client/src/components/AddAmount.js
+++ client/src/components/AddAmount.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import gql from 'graphql-tag';
-// import { Mutation } from 'react-apollo';
 import { Mutation } from '@apollo/react-components';
+import { useAlert } from 'react-alert';
 
 const ADD_AMOUNT_QUERY = gql`
         mutation AdBAmount($amount: Float!) {
@@ -14,27 +14,71 @@ const ADD_AMOUNT_QUERY = gql`
 
 const AddAmount = () => {
     let input;
+    const alert = useAlert();
+
+    const showNotification = (data) => {
+        let status = data.statusCode;
+        let hint = data.hint;
+
+        if (status === 200) {
+            alert.show(status + "/ " + hint, {
+                timeout: 4000,
+                type: 'success',
+            });
+        } else {
+            alert.show(status + "/ " + hint, {
+                timeout: 4000,
+                type: 'error',
+            });
+        }        
+    }
 
     return (
+        // mutation request with its form
         <Mutation mutation={ADD_AMOUNT_QUERY}>
-            {(addAmount, { data }) => (
-                <div>
-                    <form
-                        onSubmit={e => {
-                        e.preventDefault();
-                        addAmount({ variables: { amount: parseFloat(input.value) } });
-                        input.value = '';
-                        }}
-                    >
-                        <input
-                        ref={node => {
-                            input = node;
-                        }}
-                        />
-                        <button type="submit">Add Amount</button>
-                    </form>
-                </div>
-            )}
+            {
+                (addAmount, error) => (
+                    <div className="my-3">
+                        <form
+                            onSubmit={e => {
+                            e.preventDefault();
+
+                            // make the mutation call
+                            addAmount({ 
+                                variables: { 
+                                    // convert amount to float
+                                    amount: parseFloat(input.value)
+                                }                                
+                            })
+                                .then(res => {
+                                    showNotification(res.data.adBAmount);
+                                })
+                                .catch(error => {
+                                    showNotification({ "statusCode": 500, "hint": "Please enter appropriate value" });
+                                })
+                            input.value = '';
+                            }}
+                        >
+                            <div className="form-group row">
+                                <div className="col-xs-3">
+                                    <label htmlFor="Amount">Amount</label>
+                                    <input
+                                        className="form-control"
+                                        ref= {
+                                            node => {
+                                                input = node;
+                                            }
+                                        }
+                                    />
+                                </div>
+                                <div className="col-xs-3 my-4">
+                                    <button style={{marginTop: '5%'}} className="btn btn-primary" type="submit">Add Amount</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                )
+            }
         </Mutation>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// optional cofiguration
+const options = {
+    position: 'bottom center',
+    timeout: 5000,
+    offset: '30px',
+    transition: 'scale'
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <AlertProvider template={AlertTemplate} {...options}>
+        <App />
+    </AlertProvider>, 
+    document.getElementById('root')
+);
+
+serviceWorker.register();
